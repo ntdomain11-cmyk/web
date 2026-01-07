ca2adf4f78fb89b19271967a3ae8f799f5c4d8a8
ntdomain11-cmyk <nt.domain11@gmail.com>
2026-01-07 12:52:10 +0530

    Update config.js

--- Admin/src/lib/config.js
+++ Admin/src/lib/config.js
@@ -1,3 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE || 'https://nt-consultancy-api.vercel.app'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE || 'https://nt-api-one.vercel.app'
 
 //export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE || 'http://localhost:5646'
+
