ef00d2f609fcc6d3166f0d9f10d95be6fa529180
ntdomain11-cmyk <nt.domain11@gmail.com>
2026-01-07 12:24:50 +0530

    Delete API directory

--- API/routes/contentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ContentController = require('../controllers/contentController')
-const { auth } = require('../middlewares/auth.js')
-
-router.get('/:page', ContentController.getPageContent)
-router.put('/:page', auth, ContentController.upsertPageContent)
-
-module.exports = router
