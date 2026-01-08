33edc69fe3eb055e117b8e2cb60e16e75c7de01e
ntdomain11-cmyk <nt.domain11@gmail.com>
2026-01-08 10:28:36 +0530

    Delete Admin directory

--- Admin/src/lib/config.js
+++
@@ -1,4 +0,0 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE || 'https://nt-api-one.vercel.app'
-
-//export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE || 'http://localhost:5646'
-
