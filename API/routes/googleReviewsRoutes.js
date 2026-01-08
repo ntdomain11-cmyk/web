37fd72580b66e1f973a35ee4b1456f5754a575dc
ntdomain11-cmyk <nt.domain11@gmail.com>
2026-01-08 10:28:28 +0530

    Delete API directory

--- API/routes/googleReviewsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/googleReviewsController')
-
-router.get('/getReviews', controller.getReviews)
-
-module.exports = router
